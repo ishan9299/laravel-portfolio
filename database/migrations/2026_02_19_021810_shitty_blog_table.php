7d1d692f1ff0a4e48c6a0d85fbb94c749d81da43
ishan <ishan365.ia@gmail.com>
2026-02-19 17:10:59 +0530

    remove incomplete code that is not needed now.

--- database/migrations/2026_02_19_021810_shitty_blog_table.php
+++ database/migrations/2026_02_19_021810_shitty_blog_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create() {
-        }
+        /* Schema::create() { */
+        /* } */
     }
 
     /**
